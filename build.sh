fab85b183ced516891562f3aa025032bdcf9163c
Vishvam Moliya <vishvam.moliya@simformsolutions.com>
2026-07-14 17:41:23 +0530

    Update mdbook version to v0.5.4 to fix mermaid compatibility

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 # Download mdbook
 echo "Downloading mdbook..."
-curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.40/mdbook-v0.4.40-x86_64-unknown-linux-gnu.tar.gz | tar -xz
+curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.5.4/mdbook-v0.5.4-x86_64-unknown-linux-gnu.tar.gz | tar -xz
 
 # Download mdbook-mermaid
 echo "Downloading mdbook-mermaid..."
